fbeed6aed2c385b82cae5f2e7681f5291c4fa54a
ThijsKprs <555367@student.fontys.nl>
2025-04-08 13:10:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@latest/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@latest/examples/jsm/loaders/GLTFLoader.js';
 
 
 
